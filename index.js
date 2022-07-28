943fb788df5525be5dbba48d7a63f690202dca05
Luisibz97 <106488695+Luisibz97@users.noreply.github.com>
2022-07-27 22:15:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 const fibonacci = num => {
     const value =  [0, 1]
 
     for ( let i = 2; i < num; i++ ) {
         value[i] = value[i - 2] + value[i - 1]
     
-    } return value;
+    } return value
 
-}
+};
 
 console.log(fibonacci(10));
